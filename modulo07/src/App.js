18dc1b2cb5ec7530343babaeb8fb36bc639ffcd6
Alanna Vanzella <alanna.vanzella@pti.org.br>
2019-12-16 18:45:55 -0200

    Add header

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {  BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
@@ -7,8 +9,9 @@ import Routes from './routes';
 function App() {
   return (
     <BrowserRouter>
-     <Routes />
-     <GlobalStyle />
+      <Header />
+      <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
